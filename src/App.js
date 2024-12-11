3ea8db29684faed011c9dad0ef3b23d1de7c9ae0
apple <apple@kdc.example.com>
2024-12-11 11:49:32 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Menu from "./components/Menu";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="container">
+    <Header />
+    <Menu />
+    <Footer />
+  </div>
+  )
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from './App'
+
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// React before 18
+// ReactDOM.render(<App />, document.getElementById("root"));
